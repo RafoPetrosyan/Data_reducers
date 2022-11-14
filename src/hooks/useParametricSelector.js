893fd3d5c9c0873849c8d480767b31091e695da0
Rafo Petrosyan <petrosyanrafo0@gmail.com>
2022-11-14 09:22:45 +0400

    R_P

--- src/hooks/useParametricSelector.js
+++ src/hooks/useParametricSelector.js
@@ -1,11 +1,9 @@
 import { useSelector } from 'react-redux';
 
-const useParametricSelector = (endpoint) => {
-    const isLoading = useSelector(({data}) => data?.[endpoint]?.loading || false);
-    const response = useSelector(({data}) => data?.[endpoint]?.response || null);
-    const error = useSelector(({data}) => data?.[endpoint]?.error || null);
-
-    return { isLoading, response, error };
-};
+const useParametricSelector = (endpoint) => ({
+    isLoading: useSelector(({data}) => data?.[endpoint]?.loading || false),
+    response: useSelector(({data}) => data?.[endpoint]?.response || null),
+    error: useSelector(({data}) => data?.[endpoint]?.error || null),
+});
 
 export default useParametricSelector;
